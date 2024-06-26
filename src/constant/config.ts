8f9e336cf4c9f6ed37366c69063265441604f02f
lorematteo <loremattteo@gmail.com>
2024-06-26 16:54:27 +0200

    evol(app): clean template

---
+++ src/constant/config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  title: 'MLMarketInsight',
+  description:
+    'MLMarketInsight empowers small investors to make better decisions by integrating quantitative finance and machine learning, providing reliable probabilistic predictions and market insights to maximize gains and minimize risks.',
+  url: 'https://mlmarketinsights.vercel.app'
+};
