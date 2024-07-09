224333ed2ec96eabf0b60598296747500bb46ebd
lorematteo <loremattteo@gmail.com>
2024-07-09 02:26:27 +0200

    evol(app): update home title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,14 @@ export default function HomePage() {
   return (
     <main>
       <section className="min-h-screen bg-dark p-12 md:p-24">
-        <div className="m-auto flex max-w-[512px] flex-col gap-4 text-center">
-          <h1 className="inline-block bg-gradient-to-r from-accent to-accentdark bg-clip-text text-transparent">
-            Hello, World!
+        <div className="m-auto flex flex-col items-center gap-4 text-center">
+          <h1 className="inline-block max-w-[512px] bg-gradient-to-r from-accent to-accentdark bg-clip-text text-2xl text-transparent md:text-4xl">
+            Simulez votre portefeuille d'investissement
           </h1>
-          <p className="text-gray">
+          <p className="max-w-[768px] text-gray">
             Explorez vos stratégies d'investissement avec notre simulateur de portefeuille
-            performant. Obtenez des projections précises de la performance de votre portefeuille,
-            analysez les facteurs de risque et trouvez la meilleure allocation d'actifs adaptée à
-            vos objectifs financiers.
+            performant. Obtenez des projections précises de la performance de votre portefeuille et
+            trouvez la meilleure allocation d'actifs adaptée à vos objectifs financiers.
           </p>
         </div>
         {/* <Image src={hero} alt="Hero" className='absolute top-0 left-0 -z-10'/> */}
